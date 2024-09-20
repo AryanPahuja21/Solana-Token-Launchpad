9e70ac7ce442bd55fc2ba4e150a48ef2d0b86a4b
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-09-21 02:58:09 +0530

    token mint logic

--- src/components/Launchpad.tsx
+++ src/components/Launchpad.tsx
@@ -1,4 +1,51 @@
+import {
+  createInitializeMint2Instruction,
+  getMinimumBalanceForRentExemptMint,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import { useWallet } from "@solana/wallet-adapter-react";
+import {
+  Transaction,
+  SystemProgram,
+  Keypair,
+  Connection,
+  clusterApiUrl,
+} from "@solana/web3.js";
+
 const Launchpad = () => {
+  const wallet = useWallet();
+  const connection = new Connection(clusterApiUrl("devnet"));
+  const MINT_SIZE = 82;
+  const programId = TOKEN_PROGRAM_ID;
+  const mintAuthority = wallet.publicKey!;
+  const freezeAuthority = wallet.publicKey!;
+  const decimals = 9;
+
+  const createToken = async () => {
+    const lamports = await getMinimumBalanceForRentExemptMint(connection);
+    const keypair = Keypair.generate();
+
+    const transaction = new Transaction().add(
+      SystemProgram.createAccount({
+        fromPubkey: wallet.publicKey!,
+        newAccountPubkey: keypair.publicKey,
+        space: MINT_SIZE,
+        lamports,
+        programId,
+      }),
+      createInitializeMint2Instruction(
+        keypair.publicKey,
+        decimals,
+        mintAuthority,
+        freezeAuthority,
+        programId
+      )
+    );
+
+    transaction.partialSign(keypair);
+    if (wallet.signTransaction) wallet.signTransaction(transaction);
+  };
+
   return (
     <div className="mt-24 flex flex-col gap-14 justify-center items-center">
       <h1 className="text-4xl font-bold">Solana Token Launchpad</h1>
@@ -24,7 +71,10 @@ const Launchpad = () => {
           className="w-80 bg-white/20 px-4 py-2 rounded-lg outline-white"
         />
       </div>
-      <button className="bg-white/10 p-4 rounded-lg hover:bg-white/20 hover:scale-105 hover:font-semibold">
+      <button
+        onClick={createToken}
+        className="bg-white/10 p-4 rounded-lg hover:bg-white/20 hover:scale-105 hover:font-semibold"
+      >
         Create a Token
       </button>
     </div>
